f525bb04bb29680f085375e180c17ec124d52a69
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-21 14:32:41 -0500

    Add player controller script, and hero to scene

---
+++ Assets/Scripts/SimplePlatformController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlatformController : MonoBehaviour {
+
+    [HideInInspector]
+    public bool facingRight = true;
+
+    [HideInInspector]
+    public bool jump = true;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded;
+    private Animator anim;
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Awake ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
